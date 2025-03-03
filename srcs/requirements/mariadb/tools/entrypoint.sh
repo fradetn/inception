2a0e73a1367e6febc33e9f5ff3c14033766a1dd9
nael <nael.fradet@gmail.com>
2025-03-03 18:09:29 +0100

    mariadb ok wp script a modif

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,39 +1,29 @@
 #!/bin/bash
 
-set -e
-
-# Initialisation de la base de données
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql
-fi
-
 # Démarrage temporaire du serveur
-mysqld_safe --mowatch \
-	--datadir=/var/lib/mysql \
-	--user=mysql \
-	--skip-networking=0 \
-	--wsrep_on=OFF \
-	--expire-logs-days=0 \
-	--general-logs=1 \
-	--max-allowed-package=64M &
+mysqld_safe &
 
-# Attente du démarrage de MySQL
-while ! mysqladmin ping --silent; do
-	sleep 1
+# Attente du démarrage de MariaDB
+echo "Attente du démarrage de MariaDB..."
+while ! mysqladmin ping -h localhost -u root --silent; do
+    sleep 1
 done
 
+echo "MariaDB démarré, configuration des accès..."
+
 # Configuration des accès
-mysql -uroot <<-EOSQL
-    SET @@SESSION.SQL_LOG_BIN=0;
-    CREATE DATABASE IF NOT EXISTS \${DB_NAME};
-    CREATE USER IF NOT EXISTS \${DB_USER}@'%' IDENTIFIED BY '\${DB_PASS}';
-    GRANT ALL PRIVILEGES ON \${DB_NAME}.* TO \${DB_USER}@'%';
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '\${DB_ROOT_PASSWORD}';
-    FLUSH PRIVILEGES;
-EOSQL
+mysql -u root -p"${DB_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+
+mysql -u root -p"${DB_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+
+mysql -u root -p"${DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO ${DB_USER}@'%';"
+
+mysql -u root -p"${DB_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+
+mysql -u root -p"${DB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 
 # Arrêt propre du serveur temporaire
-mysqladmin -uroot -p\${DB_ROOT_PASSWORD} shutdown
+mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
 
 # Exécution du serveur en avant-plan
 exec mysqld_safe --datadir=/var/lib/mysql
